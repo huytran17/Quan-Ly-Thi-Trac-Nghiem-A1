ba48bf87c5d619d9ba38044ccd104dfa1af78415
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-08-18 14:25:17 +0700

    completed!

--- views/common/Error.php
+++ views/common/Error.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Error</title>
-</head>
-<body>
-    <div class="container">
-        <div>
-            <p>Đã xảy ra lỗi, vui lòng thử lại sau.</p>
-        </div>
+<div class="container">
+    <div>
+        <p>Đã xảy ra lỗi, vui lòng thử lại sau.</p>
     </div>
-</body>
-</html>
+</div>
